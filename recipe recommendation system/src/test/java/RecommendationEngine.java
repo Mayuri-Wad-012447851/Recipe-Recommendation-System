bb23d64e69549abbeccd09cfb685ac273b36f02f
Mayuri <mayuri@users.noreply.github.com>
2018-04-24 09:14:50 -0700

    changes

--- recipe recommendation system/src/test/java/RecommendationEngine.java
+++ recipe recommendation system/src/test/java/RecommendationEngine.java
@@ -1,9 +1,7 @@
 import org.apache.spark.SparkConf;
 import org.apache.spark.SparkContext;
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.SQLContext;
-import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 
@@ -12,22 +10,22 @@ public class RecommendationEngine {
 	public static final String recipePath = "./src/main/resources/full_format_recipes.json/full_format_recipes.json";
 	
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		//JavaSparkContext jsc = new JavaSparkContext("local", "Recommendation Engine");
 		//SQLContext sqlContext = new SQLContext(jsc);
 		
 		
 		SparkSession spark = SparkSession
 		        .builder()
-		        .appName("Recipe Recommendation System")
+		        .appName("Sample")
 		        .master("local[2]")
 		        .getOrCreate();
 		
 		/**
 		 * Load Recipe data
 		 */
-		JavaRDD<Row> recipeRDD = spark.read().json(recipePath).toJavaRDD();
+		Dataset<Row> people = spark.read().json("examples/src/main/resources/people.json");
 		
-		System.out.println(recipeRDD.first());
 	}
 
 }
